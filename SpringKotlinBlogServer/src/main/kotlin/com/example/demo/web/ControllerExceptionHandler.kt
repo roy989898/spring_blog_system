da8360e2b49eb927aab788857e1b3f1d36c5828f
royuuu <roy989898@gmail.com>
2020-09-30 19:02:44 +0800

    finish ControllerExceptionHandler

---
+++ SpringKotlinBlogServer/src/main/kotlin/com/example/demo/web/ControllerExceptionHandler.kt
@@ -0,0 +1,55 @@
+package com.example.demo.web
+
+import com.example.demo.NotFoundException
+import org.slf4j.LoggerFactory
+import org.springframework.core.annotation.AnnotationUtils
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.servlet.ModelAndView
+import javax.servlet.http.HttpServletRequest
+
+
+@ControllerAdvice
+
+class ControllerExceptionHandler {
+
+    private val LOGGER = LoggerFactory.getLogger(ControllerExceptionHandler::class.java)
+
+    /*  @ExceptionHandler(value = [NotFoundException::class])
+      fun handleNotFoundException(request: HttpServletRequest, e: Exception, model: Model): ModelAndView {
+          LOGGER.error("Request Url :${request.requestURI}, Exception: ${e.message}")
+          *//*  if (AnnotationUtils.findAnnotation(e.javaClass,
+                          ResponseStatus::class.java) != null) {
+              throw e
+          }*//*
+        val mav = ModelAndView();
+        mav.addObject("url", request.requestURI)
+        mav.addObject("exception", e)
+
+
+        mav.viewName = "error/error"
+        return mav
+
+    }*/
+
+    @ExceptionHandler(value = [Exception::class])
+    fun handleException(request: HttpServletRequest, e: Exception, model: Model): ModelAndView {
+        LOGGER.error("Request Url :${request.requestURI}, Exception: ${e.message}")
+//        TODO handle the @ResponseStatus
+        if (AnnotationUtils.findAnnotation(e.javaClass,
+                        ResponseStatus::class.java) != null) {
+            throw e
+        }
+        val mav = ModelAndView();
+        mav.addObject("url", request.requestURI)
+        mav.addObject("exception", e)
+
+
+        mav.viewName = "error/error"
+        return mav
+
+    }
+
+}
---
+++ SpringKotlinBlogServer/src/main/kotlin/com/example/demo/web/IndexController.kt
@@ -0,0 +1,19 @@
+package com.example.demo.web
+
+import com.example.demo.NotFoundException
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class IndexController {
+
+    @GetMapping("/")
+    fun index(): String {
+
+        val blog = null
+        if (blog == null) {
+            throw NotFoundException("Blog not found")
+        }
+        return "index"
+    }
+}
