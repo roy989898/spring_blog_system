2a602f22a50f55ab74097217e57bdf51a9d08ec9
royuuu <roy989898@gmail.com>
2020-10-02 16:43:24 +0800

    add types-input

--- SpringKotlinBlogServer/src/main/kotlin/com/example/demo/web/IndexController.kt
+++ SpringKotlinBlogServer/src/main/kotlin/com/example/demo/web/IndexController.kt
@@ -1,22 +1,22 @@
 package com.example.demo.web
 
-import com.example.demo.NotFoundException
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
-/*
 @Controller
 class IndexController {
 
     @GetMapping("/")
     fun index(): String {
 
-        */
-/* val blog = null
-         if (blog == null) {
-             throw NotFoundException("Blog not found")
-         }*//*
 
-        return "admin/blogs_input"
+        return "admin/types"
     }
-}*/
+
+    @GetMapping("/try")
+    fun index2(): String {
+
+
+        return "admin/types-input"
+    }
+}
