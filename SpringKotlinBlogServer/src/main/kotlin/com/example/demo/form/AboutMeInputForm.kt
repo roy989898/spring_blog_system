b363cae18b363b7ce379f037fa8da08e5a7945e1
royuuu <roy989898@gmail.com>
2020-10-16 15:22:51 +0800

    AboutMeController can get the form information

---
+++ SpringKotlinBlogServer/src/main/kotlin/com/example/demo/form/AboutMeInputForm.kt
@@ -0,0 +1,9 @@
+package com.example.demo.form
+
+data class AboutMeInputForm(
+        val email: String?,
+        val phone: String?,
+        val aboutMe: String?,
+        val hiddenPictureInput: String?
+
+)
--- SpringKotlinBlogServer/src/main/kotlin/com/example/demo/web/admin/AboutMeController.kt
+++ SpringKotlinBlogServer/src/main/kotlin/com/example/demo/web/admin/AboutMeController.kt
@@ -1,13 +1,26 @@
 package com.example.demo.web.admin
 
+import com.example.demo.form.AboutMeInputForm
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
+@RequestMapping("/admin")
 class AboutMeController {
 
     @GetMapping("/aboutMeAdmin")
     fun aboutMe(): String {
+
+        return "admin/about_me_input"
+
+    }
+
+
+    @PostMapping("/aboutMeAdmin")
+    fun aboutMeInput(aboutMeInputForm: AboutMeInputForm): String {
+//        TODO
 
         return "admin/about_me_input"
 
