04b876d92f8633560da883b5042fa921f5a7ad5e
royuuu <roy989898@gmail.com>
2020-11-22 18:02:19 +0800

    finish addBlog

--- SpringKotlinBlogServer/src/main/kotlin/com/example/demo/api/AdminApiController.kt
+++ SpringKotlinBlogServer/src/main/kotlin/com/example/demo/api/AdminApiController.kt
@@ -5,11 +5,18 @@ import com.example.demo.errorHandle.RestAPi.DefaultError
 import com.example.demo.errorHandle.RestErrorResponse
 import com.example.demo.form.BlogInputForm
 import com.example.demo.form.BlogSearchForm
+import com.example.demo.po.Tag
 import com.example.demo.service.BlogService
+import com.example.demo.service.TagService
+import com.example.demo.service.TypeService
+import com.example.demo.service.UserService
+import com.example.demo.unwrap
 import org.slf4j.LoggerFactory
 import org.springframework.data.domain.Sort
 import org.springframework.http.HttpStatus
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.context.request.WebRequest
@@ -21,9 +28,15 @@ import javax.validation.Valid
 
 @RestController()
 @RequestMapping("/api/admin")
-class AdminApiController(private val blogService: BlogService) {
+class AdminApiController(private val blogService: BlogService, private val tagService: TagService, private val userService: UserService, private val typeService: TypeService) {
 
     private val LOGGER = LoggerFactory.getLogger(AdminApiController::class.java)
+
+
+    private fun getUser(): UsernamePasswordAuthenticationToken {
+        val up = SecurityContextHolder.getContext().authentication as UsernamePasswordAuthenticationToken
+        return up
+    }
 
     @ExceptionHandler(Exception::class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
@@ -53,29 +66,58 @@ class AdminApiController(private val blogService: BlogService) {
 
         }
 
-
-        @PreAuthorize("hasRole('USER')")
-        @PostMapping("/blogs/search")
-        fun searchBlog(blogForm: BlogSearchForm): List<GetBlogListResponse> {
-            val sort = Sort.by(Sort.Direction.DESC, "updateTime")
-            val blogs = blogService.listBlog(blogForm, sort)
-            val result = blogs.map {
-                GetBlogListResponse(it.id, it.title, it.recommend, it.published, it.updateTime)
+        val user = getUser()
+        val dbUser = userService.getUser(user.name).unwrap()
+        if (dbUser == null) {
+            throw RuntimeException("User not found")
+        } else {
+            val newBlog = blogInputForm.toBlog()
+            val tagStringArray = blogInputForm.tags?.split('_')?.toHashSet()?.toList() ?: emptyList()
+            val tagList = tagStringArray.map {
+                return@map Tag(null, it, emptyList())
             }
-            return result
-        }
+            val vTags = tagList.map {
+                val gotTag = tagService.getTag(it.name).unwrap()
 
-        @PreAuthorize("hasRole('USER')")
-        @GetMapping("/blogs")
-        fun getBlogList(): List<GetBlogListResponse> {
-            val sort = Sort.by(Sort.Direction.DESC, "updateTime")
-            val blogs = blogService.listBlog(sort)
-            val result = blogs.map {
-                GetBlogListResponse(it.id, it.title, it.recommend, it.published, it.updateTime)
+                return@map gotTag ?: it
             }
-            return result
+//
+            newBlog.tags = vTags.toMutableSet()
+            newBlog.user = dbUser
+            val type = blogInputForm.typeId?.let {
+                typeService.getType(it)
+            }
+            type?.let {
+                newBlog.type = it
+            }
+            blogService.saveBlog(newBlog)
         }
 
 
     }
+
+
+    @PreAuthorize("hasRole('USER')")
+    @PostMapping("/blogs/search")
+    fun searchBlog(blogForm: BlogSearchForm): List<GetBlogListResponse> {
+        val sort = Sort.by(Sort.Direction.DESC, "updateTime")
+        val blogs = blogService.listBlog(blogForm, sort)
+        val result = blogs.map {
+            GetBlogListResponse(it.id, it.title, it.recommend, it.published, it.updateTime)
+        }
+        return result
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/blogs")
+    fun getBlogList(): List<GetBlogListResponse> {
+        val sort = Sort.by(Sort.Direction.DESC, "updateTime")
+        val blogs = blogService.listBlog(sort)
+        val result = blogs.map {
+            GetBlogListResponse(it.id, it.title, it.recommend, it.published, it.updateTime)
+        }
+        return result
+    }
+
+
 }
